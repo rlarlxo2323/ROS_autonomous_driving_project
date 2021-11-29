e40f898530fe62f05773d20aee9168d9979246aa
DuHyeon2 <engus4022@naver.com>
2021-11-29 07:14:02 +0000

    add file Obstacle

---
+++ scripts/ros_car/Detect_Obstacle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
+from drive_controller import RobotDriveController
+
+class Find_obstacle():
+
+    def __init__(self):
+        self.range_ahead = 0
+        self.range_right = 0
+        self.detect_obstacle_pub = rospy.Publisher('obstacle_pub', Bool, queue_size=1)
+        self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
+        self.twist = Twist()
+        self.drive_controller = RobotDriveController()
+        self.detect_obstacle = True
+
+    def scan_callback(self, msg):
+        rospy.loginfo(msg.ranges[180])
+        if 1 < msg.ranges[180] < 2:
+            rospy.loginfo("Detect_Obstacle")
+            self.detect_obstacle = False
+        else:
+            self.detect_obstacle = True
+
+
+    def drive(self):
+        if self.detect_obstacle:
+            self.drive_controller.set_velocity(1)
+        else:
+            self.drive_controller.set_velocity(0)
+        self.drive_controller.drive()
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('test_node')
+    detecter = Find_obstacle
+    while not rospy.is_shutdown():
+        detecter.drive()
+        detecter.rate.sleep()
